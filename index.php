0e0fd35e72b17b1bc6da90f7244b5635ee71c990
ErikssonHerlo <erikssonhernandez201830459@cunoc.edu.gt>
2021-12-08 05:55:20 -0600

    Studying Php
    Hello World on Php

---
+++ index.php
@@ -0,0 +1,60 @@
+<?<php>
+<html lang="es" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title>Super Calculadora</title>
+    <link rel="stylesheet" href="style.css">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   </head>
+<body>
+ <header>
+   <nav>
+   <div class="navbar">
+     <div class="logo">
+       <img src="resources/logousac.png" alt="">
+     </div>
+     <ul class="menu">
+      <li><a href="#">Inicio</a></li>
+       <li><a href="https://github.com/ErikssonHerlo" target="_blank">Servicios</a></li>
+       <li><a href="#">Acerca De</a></li>
+      <li><a href="#">Contáctanos</a></li>
+      </ul>
+      <div class="search-box">
+        <input type="text" placeholder="Buscar...">
+        <a href="#"><i class="fas fa-search"></i></a>
+      </div>
+   </div>
+   </nav>
+   <div class="content">
+     <div class="text-content">
+       <div class="text">Hola, Soy</div>
+       <div class="name">Eriksson Hernández</div>
+       <div class="job">
+         <div class="job">
+           <span>Soy un</span>
+            <div class="typing-text">
+              <span class="one">Frontend &</span>
+              <span class="two">Backend Developer.</span>
+            </div>
+          </div>
+       </div>
+       <div class="buttons">
+   <button>Sobre Mí</button>
+   <button>Sigueme para más</button>
+</div>
+     </div>
+     <div class="girl">
+       
+     </div>
+   </div>
+   
+   <div class="media-icons">
+    <a href="https://www.facebook.com/eriksson.hernandez/" target="_blank"><i class="fab fa-facebook-f"></i></a>
+    <a href="https://www.instagram.com/erikssonherlo/"><i class="fab fa-instagram" target="_blank"></i></a>
+    <a href="https://github.com/ErikssonHerlo"><i class="fab fa-github" target="_blank"></i></a>
+    <a href="https://twitter.com/Eriksson_Herlo"><i class="fab fa-twitter" target="_blank"></i></a>
+  </div>
+ </header>
+</body>
+</php>
